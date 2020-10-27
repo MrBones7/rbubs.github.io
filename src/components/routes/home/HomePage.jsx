9ab905f15a0b812e5d1131adce8209129f882c20
rbubs <rbubs>
2020-10-27 12:58:32 -0700

    Add SupportButton

--- src/components/routes/home/HomePage.jsx
+++ src/components/routes/home/HomePage.jsx
@@ -1,12 +1,14 @@
 /* eslint-disable jsx-a11y/media-has-caption */
 import React from 'react';
+import PropTypes from 'prop-types';
 import cx from 'classnames';
-import { fullscreen, backgroundVideoPlayer, ui } from './HomePage.styles.less';
+import { row, fullscreen, backgroundVideoPlayer, ui } from './HomePage.styles.less';
 import Nav from '../../ui/Nav';
 import Player from '../../ui/Player';
-import Counter from '../../ui/Counter';
+import SupportButton from '../../ui/SupportButton';
 import BackgroundVideoPlayer from '../../ui/BackgroundVideo';
 
+const supportUrl = 'https://app.rarible.com/token/0xd07dc4262bcdbf85190c01c996b4c06a461d2430:59031:0x6667c694b55e68f6409bc96ace9a9ac3bb1fc132';
 const videoSrc = [
   {
     src: 'https://rebase.radio/assets/background-vp9.webm',
@@ -22,12 +24,27 @@ const stationId = 'sb5955894a';
 
 const Spacer = () => <div style={{ flexGrow: 1 }} />;
 
+const Row = ({ alignItems, children }) => (
+  <div className={row} style={{ alignItems }}>{children}</div>
+);
+Row.propTypes = {
+  alignItems: PropTypes.string,
+  children: PropTypes.any.isRequired,
+};
+
 const HomePage = () => (
   <>
     <div className={cx(fullscreen, ui)}>
-      <Nav />
+      <Row>
+        <Nav />
+      </Row>
       <Spacer />
-      <Player initialVolume={0.6} stationId={stationId} />
+      <Row alignItems="flex-end">
+        <Player initialVolume={0.6} stationId={stationId} />
+        <SupportButton link={supportUrl}>
+          Support Us
+        </SupportButton>
+      </Row>
     </div>
     <BackgroundVideoPlayer
       src={videoSrc}
---
+++ src/components/ui/SupportButton/index.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import cx from 'classnames';
+import PropTypes from 'prop-types';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faHeart } from '@fortawesome/free-solid-svg-icons';
+import { icon, supportButton, animated } from './styles.less';
+
+const SupportButton = ({ link, children }) => (
+  <a className={cx(supportButton)} target="_blank" rel="noopener noreferrer" href={link}>
+    <span className={animated}>{children}</span>
+    <FontAwesomeIcon icon={faHeart} className={icon} />
+  </a>
+);
+SupportButton.propTypes = {
+  link: PropTypes.string.isRequired,
+  children: PropTypes.any.isRequired,
+};
+
+export default SupportButton;
