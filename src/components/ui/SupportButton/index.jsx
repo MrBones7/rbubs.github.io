b0ce3bd92c5a06f3def56d651df8a4269f99a45c
dtolsen <dtolsen@protonmail.com>
2021-03-30 13:36:26 -0600

    Additional changes for dynamic background videos

--- src/components/ui/SupportButton/index.jsx
+++ src/components/ui/SupportButton/index.jsx
@@ -5,15 +5,18 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHeart } from '@fortawesome/free-solid-svg-icons';
 import { icon, supportButton } from './styles.less';
 
-const SupportButton = ({ link }) => (
-  <a className={cx(supportButton)} target="_blank" rel="noopener noreferrer" href={link}>
-    {/*<span className={animated}>{children}</span>*/}
-    <FontAwesomeIcon icon={faHeart} className={icon} />
-  </a>
-);
+const SupportButton = ({link}) => {
+  return link && (
+    <a className={cx(supportButton)} target="_blank" rel="noopener noreferrer" href={link}>
+      {/*<span className={animated}>{children}</span>*/}
+      <FontAwesomeIcon icon={faHeart} className={icon} />
+    </a>  
+  )
+}
+
 SupportButton.propTypes = {
-  link: PropTypes.string.isRequired,
+  link: PropTypes.string,
   // children: PropTypes.any.isRequired,
-};
+}
 
 export default SupportButton;
