fcab58642d0a3b813bc15e5938c82fcf954824b0
dtolsen <dtolsen@protonmail.com>
2021-04-06 17:49:18 -0600

    Troubleshooting between scene timing issue

--- src/components/ui/BackgroundVideo/index.jsx
+++ src/components/ui/BackgroundVideo/index.jsx
@@ -58,18 +58,19 @@ class BackgroundVideoPlayer extends Component {
     player.pause();
     player.addEventListener("canplay", function onCanPlay() {
       player.removeEventListener("canplay", onCanPlay);
-      player.play();
-      // short delay to remove potential loading flash between scenes
-      if (this.state.prevPlayer !== undefined) {
-        this.videoTimer = setTimeout(() =>  {
-          prevPlayer = document.querySelector(`.${this.state.prevPlayer}`);
-          if (prevPlayer) {
-            prevPlayer.style.display = "none";
-          }
-        }, 450);
-      }      
+      player.play();  
     });
     player.load();
+
+    // short delay to remove potential loading flash between scenes
+    if (this.state.prevPlayer !== undefined) {
+      this.videoTimer = setTimeout(() =>  {
+        prevPlayer = document.querySelector(`.${this.state.prevPlayer}`);
+        if (prevPlayer) {
+          prevPlayer.style.display = "none";
+        }
+      }, 450);
+    }    
 
     // call handleVideoEnd when each video ends
     player.addEventListener('ended', this.handleVideoEnd, false);   
