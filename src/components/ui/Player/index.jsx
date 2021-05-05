14f739a962376b0f71d8cd1dd8338fdcee07eda5
dtolsen <dtolsen@protonmail.com>
2021-05-05 16:33:22 -0600

    Style tweaks

--- src/components/ui/Player/index.jsx
+++ src/components/ui/Player/index.jsx
@@ -129,7 +129,7 @@ class Player extends React.Component {
 
             <div>
               <svg width="32" height="32" viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <circle cx="16" cy="16" r="14.5" stroke="white" stroke-width="3"/>
+                <circle cx="16" cy="16" r="14.5" stroke="white" strokeWidth="3"/>
               </svg>
               <p>Coming Soon</p>
             </div>
